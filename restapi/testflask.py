f0a7f4db037f883062065f4f462c4dec8a9cb1cf
danburkhol <agentdax5@users.noreply.github.com>
2016-02-06 05:00:35 -0500

    working DB access from a named search
    and initial returning JSON

--- restapi/testflask.py
+++ restapi/testflask.py
@@ -1,3 +1,8 @@
+#DB Schema
+#0	Streetname		3   name       6	Latitude
+#1  Address Number	4	Rating
+#2  ZIP				5   Longitude
+
 from flask import Flask, request
 from flask_restful import Resource, Api
 
@@ -42,21 +47,50 @@ class SearchByName(Resource):
 		recvName = request.form['name']
 
 		
-		nameQuery = "SELECT * FROM locations WHERE LOCATIONAME = " + recvName
+		nameQuery = "SELECT * FROM locations WHERE LOCATIONNAME = "
+		nameQuery += "\'"
+		nameQuery += recvName
+		nameQuery += "\'"
+
+		print nameQuery
 
 
 		con = _mysql.connect(host="localhost", user="root", passwd="code4good", db="team4")
 		con.query(nameQuery)
 
-		results = con.use_result()
+		results = con.store_result()
+
+		rows = results.fetch_row()
+		print rows
 
 		data = {}
+		
 
-		for result in results:
-			data['name'] = result['name']
-			print result['name']
+		for row in rows: 
+			dataItem = {}
 
-		return data
+			dataItem['streetname'] = row[0]
+			dataItem['addressnumber'] = row[1]
+			dataItem['zip'] = row[2]
+			dataItem['name'] = row[3]
+			dataItem['rating'] = row[4]
+			dataItem['longitude'] = row[5]
+			dataItem['latitude'] = row[6]
+
+			json_data = json.dumps(dataItem);
+			data += json_data
+
+		print data
+
+		json_data_final = json.dumps(data)
+
+		print json_data_final
+
+		print rows
+
+		return json_data_final
+		
+
 
 	#curl --data "param1=value1&param2=value2" https://example.com/resource.cgi
 	#curl --data "name=starbucks" http://127.0.0.1:5000/searchName
@@ -83,7 +117,7 @@ class SearchByLocation(Resource):
 		logSearchMin = log - 0.5000
 		latSearchMin = lat - 0.5000
 
-		locationQuery = "SELECT * from location WHERE ((LONGITUDE BETWEEN " + logSearchPlus + " AND " + logSearchMin ") AND (LATITUDE BETWEEN " + latSearchPlus + " AND " + latSearchMin + "))"
+#		locationQuery = "SELECT * from location WHERE ((LONGITUDE BETWEEN " + logSearchPlus + " AND " + logSearchMin ") AND (LATITUDE BETWEEN " + latSearchPlus + " AND " + latSearchMin + "))"
 
 		con = _mysql.connect(host="localhost", user="root", passwd="code4good", db="team4")
 		con.query(locationQuery)
@@ -103,6 +137,7 @@ class SearchByLocation(Resource):
 api.add_resource(HelloWorld, '/')
 api.add_resource(ConnectDBTest, '/DBT')
 api.add_resource(SearchByLocation, '/searchLoc')
+api.add_resource(SearchByName, '/searchName')
 
 if __name__ == '__main__':
 	app.run(debug=True)
