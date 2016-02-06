2f3f8b83777ef567cb7b57615d7c6dc8a9b88d48
Adam Martinez <adaricmar@gmail.com>
2016-02-05 23:39:24 -0500

    phptest

---
+++ search.php
@@ -0,0 +1,27 @@
+<?php
+
+$servername = "localhost";
+$username = "root";
+$password = "code4good";
+
+$conn = new mysqli($servername, $username, $password);
+
+if ($conn->connect_error) {
+   die("Connection failed: " . $conn->connect_error);
+}
+
+
+
+$sql = "SELECT $querytype from locations where $querytype=$query";
+$result = $conn->query($sql);
+
+if ($result->num_rows > 0) {
+    // output data of each row
+    while($row = $result->fetch_assoc()) {
+        echo "Name: " . $row["LOCATIONNAME"]. "<br>";
+    }
+} else {
+    echo "0 results";
+}
+$conn->close();
+?> 
