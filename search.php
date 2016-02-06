7bcd0bb5e51f54c0c5a0499d3d59064975384dc4
Adam Martinez <adaricmar@gmail.com>
2016-02-06 00:19:10 -0500

    phptest

--- search.php
+++ search.php
@@ -1,34 +1,120 @@
-<?php
+<!DOCTYPE html>
+<html class="full" lang="en">
+<!-- Make sure the <html> tag is set to the .full CSS class. Change the background image in the full.css file. -->
 
-$servername = "localhost";
-$username = "root";
-$password = "code4good";
-$dbname = "team4";
+<head>
 
-$query = $_POST["query"];
-$querytype = $_POST["querytype"];
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="description" content="">
+    <meta name="author" content="">
+
+    <title>The Big Picture - Start Bootstrap Template</title>
+
+    <!-- Bootstrap Core CSS -->
+    <link href="css/bootstrap.min.css" rel="stylesheet">
+
+    <!-- Custom CSS -->
+    <link href="css/the-big-picture.css" rel="stylesheet">
+
+    <!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
+    <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+    <!--[if lt IE 9]>
+        <script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
+        <script src="https://oss.maxcdn.com/libs/respond.js/1.4.2/respond.min.js"></script>
+    <![endif]-->
+
+</head>
+
+<body>
+
+    <!-- Navigation -->
+    <nav class="navbar navbar-inverse navbar-fixed-bottom" role="navigation">
+        <div class="container">
+            <!-- Brand and toggle get grouped for better mobile display -->
+            <div class="navbar-header">
+                <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1">
+                    <span class="sr-only">Toggle navigation</span>
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                </button>
+                <a class="navbar-brand" href="#">The Big Picture</a>
+            </div>
+            <!-- Collect the nav links, forms, and other content for toggling -->
+            <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+                <ul class="nav navbar-nav">
+                   	<li>
+                        <a href="about.html">Find</a>
+                    </li>
+                    <li>
+                        <a href="services.html">Services</a>
+                    </li>
+                    <li>
+                        <a href="contact.html">Contact</a>
+                    </li>
+                </ul>
+            </div>
+            <!-- /.navbar-collapse -->
+        </div>
+        <!-- /.container -->
+    </nav>
+
+    <!-- Page Content -->
+    <div class="container">
+        <div class="row">
+            <div class="col-md-6 col-sm-12">
+                <h1>The Big Picture</h1>
+		       
+		<?php
+
+		$servername = "localhost";
+		$username = "root";
+		$password = "code4good";
+		$dbname = "team4";
+
+		$query = $_POST["query"];
+		$querytype = $_POST["querytype"];
 
 
-$conn = new mysqli($servername, $username, $password, $dbname);
+		$conn = new mysqli($servername, $username, $password, $dbname);
 
-if ($conn->connect_error) {
-   die("Connection failed: " . $conn->connect_error);
-}
-
-
-
-$sql = "SELECT * from locations where $querytype = $query";
+		if ($conn->connect_error) {
+		   die("Connection failed: " . $conn->connect_error);
+		}
 
 
-$result = $conn->query($sql);
 
-if ($result->num_rows > 0) {
-    // output data of each row
-    while($row = $result->fetch_assoc()) {
-        echo "Name: " . $row["LOCATIONNAME"]. "<br>";
-    }
-} else {
-    echo "0 results";
-}
-$conn->close();
-?> 
+		$sql = "SELECT * from locations where $querytype = $query";
+
+
+		$result = $conn->query($sql);
+
+		if ($result->num_rows > 0) {
+		    // output data of each row
+		    while($row = $result->fetch_assoc()) {
+			echo "Name: " . $row["LOCATIONNAME"]. "<br>";
+		    }
+		} else {
+		    echo "0 results";
+		}
+		$conn->close();
+		?> 
+
+
+            </div>
+        </div>
+        <!-- /.row -->
+    </div>
+    <!-- /.container -->
+
+    <!-- jQuery -->
+    <script src="js/jquery.js"></script>
+
+    <!-- Bootstrap Core JavaScript -->
+    <script src="js/bootstrap.min.js"></script>
+
+</body>
+
+</html>
