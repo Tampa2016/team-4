7b12b80560a0e6b8c7a49b97e3236c83e0412a90
danburkhol <agentdax5@users.noreply.github.com>
2016-02-06 03:47:29 -0500

    search by name init

--- restapi/testflask.py
+++ restapi/testflask.py
@@ -1,6 +1,8 @@
 from flask import Flask, request
 from flask_restful import Resource, Api
 
+import json
+
 DEBUG = True
 
 import _mysql
@@ -34,16 +36,25 @@ class ConnectDBTest(Resource):
 		return {'TestDB': 'Successful'}
 
 
-class AddDB(Resource):
-	def get(self):
+class SearchByName(Resource):
+	def put(self, name):
+		recvName = request.form['data']
+		data = {}
 
+		nameQuery = "SELECT * from location WHERE LOCATIONAME = " + recvName
 
+		con = _mysql.connect(host="localhost", user="root", passwd="code4good", db="team4")
+		con.query(nameQuery)
 
-class AddAttributes(Resource):
-	def put(self, todo_id):
+		results = con.use_result()
 
+		for result in results:
+			data['name'] = result['name']
+			print result['name']
 
+		return data
 
+#TODO DEBUG MULTIPLE VAR INPUT JSON
 class SearchByLocation(Resource):
 	def put(self):
 		#Get location Long / Lat by JSON Object from HTTP PUT Req
@@ -62,7 +73,7 @@ class SearchByLocation(Resource):
 		logSearchMin = log - 0.5000
 		latSearchMin = lat - 0.5000
 
-		locationQuery = "SELECT * from location WHERE ((LONGITUDE BETWEEN logSearchPlus AND logSearchMin) AND (LATITUDE BETWEEN latSearchPlus AND latSearchMin))"
+		locationQuery = "SELECT * from location WHERE ((LONGITUDE BETWEEN " + logSearchPlus + " AND " + logSearchMin ") AND (LATITUDE BETWEEN " + latSearchPlus + " AND " + latSearchMin + "))"
 
 		con = _mysql.connect(host="localhost", user="root", passwd="code4good", db="team4")
 		con.query(locationQuery)
@@ -81,7 +92,6 @@ class SearchByLocation(Resource):
 
 api.add_resource(HelloWorld, '/')
 api.add_resource(ConnectDBTest, '/DBT')
-api.add_resource(AddDB, '/add')
 api.add_resource(SearchByLocation, '/searchLoc')
 
 if __name__ == '__main__':
