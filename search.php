a7a46d643045dbfb92cd663e66128e1a0205abbb
mejo123 <stephenastf@gmail.com>
2016-02-06 02:58:16 -0500

    Updated search.php and cleanup unused file

--- search.php
+++ search.php
@@ -10,13 +10,13 @@
     <meta name="description" content="">
     <meta name="author" content="">
 
-    <title>The Big Picture - Start Bootstrap Template</title>
+    <title>Full - Start Bootstrap Template</title>
 
     <!-- Bootstrap Core CSS -->
     <link href="css/bootstrap.min.css" rel="stylesheet">
 
     <!-- Custom CSS -->
-    <link href="css/the-big-picture.css" rel="stylesheet">
+    <link href="css/full.css" rel="stylesheet">
 
     <!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
     <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
@@ -30,7 +30,7 @@
 <body>
 
     <!-- Navigation -->
-    <nav class="navbar navbar-inverse navbar-fixed-bottom" role="navigation">
+    <nav class="navbar navbar-inverse navbar-fixed-top" role="navigation">
         <div class="container">
             <!-- Brand and toggle get grouped for better mobile display -->
             <div class="navbar-header">
@@ -40,19 +40,19 @@
                     <span class="icon-bar"></span>
                     <span class="icon-bar"></span>
                 </button>
-                <a class="navbar-brand" href="#">The Big Picture</a>
+                <a class="navbar-brand" href="index.html">Locate</a>
             </div>
             <!-- Collect the nav links, forms, and other content for toggling -->
             <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
                 <ul class="nav navbar-nav">
-                   	<li>
-                        <a href="about.html">Find</a>
+                    <li>
+                        <a href="#">Rate</a>
                     </li>
                     <li>
-                        <a href="services.html">Services</a>
+                        <a href="#">Services</a>
                     </li>
                     <li>
-                        <a href="contact.html">Contact</a>
+                        <a href="#">Contact</a>
                     </li>
                 </ul>
             </div>
@@ -61,56 +61,45 @@
         <!-- /.container -->
     </nav>
 
-    <!-- Page Content -->
-    <div class="container">
+    <div class="container" >
         <div class="row">
             <div class="col-md-6 col-sm-12">
-                <h1>The Big Picture</h1>
-		       
-		<?php
+                <?php
+	     $servername = "localhost";
+	     $username = "root";
+	     $password = "code4good";
+	     $dbname = "myDB";
 
-		$servername = "localhost";
-		$username = "root";
-		$password = "code4good";
-		$dbname = "team4";
+	     // Create connection
+	     $conn = new mysqli($servername, $username, $password, $dbname);
+	     // Check connection
+	     if ($conn->connect_error) {
+                die("Connection failed: " . $conn->connect_error);
+                }
 
-		$streetname = $_POST["STREETNAME"];
-		$addressnumber = $_POST["ADDRESSNUMBER"];
-		$zip = $_POST["ZIP"];
-		$locationname = $_POST["LOCATIONNAME"];
-		$rating = $_POST["RATING"];
+                $sql = "SELECT id, firstname, lastname FROM MyGuests";
+                $result = $conn->query($sql);
 
-
-		$conn = new mysqli($servername, $username, $password, $dbname);
-
-		if ($conn->connect_error) {
-		   die("Connection failed: " . $conn->connect_error);
-		}
-
-
-		echo $rating;
-
-
-
-		$sql = "INSERT INTO locations (STREETNAME, ADDRESSNUMBER, ZIP, LOCATIONNAME, RATING) VALUES ('$streetname', $addressnumber, $zip, '$locationname', $rating)";
-
-		if ($conn->query($sql) === TRUE) {
-  		  echo "New record created successfully";
-		} else {
-		    echo "Error: " . $sql . "<br>" . $conn->error;
-		}
-
-		
-		$conn->close();
-		?> 
-
-
+                if ($result->num_rows > 0) {
+                // output data of each row
+                while($row = $result->fetch_assoc()) {
+                echo "id: " . $row["id"]. " - Name: " . $row["firstname"]. " " . $row["lastname"]. "<br>";
+                }
+                } else {
+                echo "0 results";
+                }
+                $conn->close();
+                ?>
             </div>
         </div>
         <!-- /.row -->
     </div>
-    <!-- /.container -->
 
+    <!-- Put your page content here! -->
+    <div id="map" style="width: 100%; height: 94%; position: absolute; vertical-align:middle;">
+        <script async defer
+                src="https://maps.googleapis.com/maps/api/js?key=AIzaSyClf55i7ai_BLWi1C__3gJsbT6CsLNuzV4&callback=initMap">
+        </script>
     <!-- jQuery -->
     <script src="js/jquery.js"></script>
 
