4751149e8233e5e297851acc9ef425c96dbcb271
danburkhol <agentdax5@users.noreply.github.com>
2016-02-06 06:17:23 -0500

    refined location function

--- restapi/testflask.py
+++ restapi/testflask.py
@@ -40,25 +40,65 @@ class ConnectDBTest(Resource):
 
 		return {'TestDB': 'Successful'}
 
+def accessDB(inputQuery):
+	con = _mysql.connect(host="localhost", user="root", passwd="code4good", db="team4")
+	con.query(nameQuery)
 
-def searchDBByName(inputName):
+	return con
+
+def searchDBName(inputName):
 
 	nameQuery = "SELECT * FROM locations WHERE LOCATIONNAME = "
 	nameQuery += "\'"
-	nameQuery += recvName
+	nameQuery += inputName
 	nameQuery += "\'"
 
 	print nameQuery
 
 
-	con = _mysql.connect(host="localhost", user="root", passwd="code4good", db="team4")
-	con.query(nameQuery)
-
-	results = con.store_result()
-
-	con.close()
+	#con = _mysql.connect(host="localhost", user="root", passwd="code4good", db="team4")
+	#con.query(nameQuery)
+	
+	#Access the DB and store result, return it
+	results = accessDB(nameQuery).store_result()
 
 	return results
+
+def searchDBLocation(log, lat):
+		#Search range in terms of longitude and lattitude
+		longSearchPlus = log + 0.5000
+		longSearchMin = log - 0.5000
+
+		latSearchPlus = lat + 0.5000
+		latSearchMin = lat - 0.5000
+
+#		locationQuery = "SELECT * from location WHERE ((LONGITUDE BETWEEN " + logSearchPlus + " AND " + logSearchMin ") AND (LATITUDE BETWEEN " + latSearchPlus + " AND " + latSearchMin + "))"
+
+		#Build the query
+		#Probably uncessary work but tried and true so far so oh well
+		locationQuery = "SELECT * from location WHERE ((LONGITUDE BETWEEN " 
+		locationQuery += "\'"
+		locationQuery += logSearchPlus
+		locationQuery += "\'"
+		locationQuery += " AND "
+		locationQuery += "\'"
+		locationQuery += logSearchMin
+		locationQuery += "\'"
+		locationQuery += ") AND LATITUDE BETWEEN "
+		locationQuery += "\'"
+		locationQuery += latSearchPlus
+		locationQuery += "\'"
+		locationQuery += " AND "
+		locationQuery += "\'"
+		locationQuery += latSearchMind
+		locationQuery += "\'"
+		locationQuery += "))"
+
+		print locationQuery
+
+		results = accessDB(locationQuery).store_result()
+
+		return results
 
 
 
@@ -86,99 +126,42 @@ def formatJSON(inputResults):
 	return json_data_final
 
 
-class SearchByName(Resource):
-	#curl -X PUT -d 'name=starbucks' http://127.0.0.1:5000/searchName
-	def put(self):
-		print request
-		recvName = request.form['name']
-
-		
-		nameQuery = "SELECT * FROM locations WHERE LOCATIONNAME = "
-		nameQuery += "\'"
-		nameQuery += recvName
-		nameQuery += "\'"
-
-		print nameQuery
-
-
-		con = _mysql.connect(host="localhost", user="root", passwd="code4good", db="team4")
-		con.query(nameQuery)
-
-		results = con.store_result()
-
-		rows = results.fetch_row()
-		print rows
-
-		dataItem = {}
-
-		for row in rows: 
-			dataItem["streetname"] = row[0]
-			dataItem["addressnumber"] = row[1]
-			dataItem["zip"] = row[2]
-			dataItem["name"] = row[3]
-			dataItem["rating"] = row[4]
-			dataItem["longitude"] = row[5]
-			dataItem["latitude"] = row[6]
-
-
-
-		json_data_final = json.dumps(dataItem)
-
-		print json_data_final
-		return json_data_final
-		
+class SearchByName(Resource):		
 	def get(set):
-		print request
+		#Example curl request
+		# curl http://127.0.0.1:5000?name=starbucks
+
+		#request comes in, grab name varable
 		recvName = request.args['name']
 
-		results = searchDBByName(recvName)
+		results = searchDBName(recvName)	
+		json_data = formatJSON(results)
 
-		return (formatJSON(results))
+		return json_data
 		
 
 
-# curl http://127.0.0.1:5000?name=starbucks
-#curl http://127.0.0.1:5000/searchName?name=starbucks
 
 #TODO DEBUG MULTIPLE VAR INPUT JSON
 class SearchByLocation(Resource):
-	def put(self):
-		#Get location Long / Lat by JSON Object from HTTP PUT Req
-		data = request.form['data']
-
-		log = data.form['log']
-		lat = data.form['lat']
-
-		print log + " " + lat
-
-		#27.986800173,	-82.328399309
-		#Search range 
-		logSearchPlus = log + 0.5000
-		latSearchPlus = lat + 0.5000
-
-		logSearchMin = log - 0.5000
-		latSearchMin = lat - 0.5000
-
-#		locationQuery = "SELECT * from location WHERE ((LONGITUDE BETWEEN " + logSearchPlus + " AND " + logSearchMin ") AND (LATITUDE BETWEEN " + latSearchPlus + " AND " + latSearchMin + "))"
-
-		con = _mysql.connect(host="localhost", user="root", passwd="code4good", db="team4")
-		con.query(locationQuery)
-
-		results = con.use_result()
-
-		for result in results:
-			print result["name"]
-
-		return {'TestByLocation': 'Successful'}
+	#Example curl request
+	# curl http://127.0.0.1:5000?longitude=27.986800173&starbucks:latitude=-82.328399309
+	def get(self):
+		longitude = request.args['longitude']
+		latitude = request.args['latitude']
 
 
-#curl -X PUT -d log=27.980000 -d lat=-82.320000 127.0.0.1:5000/searchLoc
-#curl -X PUT -d log=27.980000 -d lat=-82.320000 127.0.0.1:5000/searchLoc
-#curl -X PUT -H "Content-Type: application/json" -d '{"log":"27.98000"}, {"lat":"-82.32000"}' 127.0.0.1:5000/searchLoc
+		results = SearchByLocation()
+		json_data = formatJSON(results)
+
+
+		return json_data
+
+
 
 api.add_resource(HelloWorld, '/')
 api.add_resource(ConnectDBTest, '/DBT')
-api.add_resource(SearchByLocation, '/searchLoc')
+api.add_resource(SearchByLocation, '/searchLocation')
 api.add_resource(SearchByName, '/searchName')
 
 if __name__ == '__main__':
